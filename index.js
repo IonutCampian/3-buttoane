d88c5b464ba712a3983454a11e3a0cf09ffa24d2
IonutCampian <87191750+IonutCampian@users.noreply.github.com>
2021-08-01 19:44:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function createButton() {
+   var button1 = document.createElement("BUTTON");
+   var text1 = document.createTextNode("1");
+   button1.appendChild(text1);
+   var button2 = document.createElement("BUTTON");
+   var text2 = document.createTextNode("2");
+   button1.appendChild(text2);
+   var button3 = document.createElement("BUTTON");
+   var text3 = document.createTextNode("3");
+   button1.appendChild(text3);
+  }
