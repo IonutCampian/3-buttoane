fdd8a5834465299d0ef80be16cfc5aa413a571c1
IonutCampian <87191750+IonutCampian@users.noreply.github.com>
2021-08-03 21:20:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
+var adaugaButoate = document.getElementById('adaugaButoate');
+var btn = [0 , 1 , 2];
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-        }
-    var numar = getRandomInt(3);
-    var button = document.getElementById('nou');
-    button.addEventListener('click', e => {
-        if( numar == 0) {
-            var text = 'winner';
-            document.write(text);
-        }
-        else{
-            alert('try again')
-        }
-    } )
-    var button1 = document.getElementById('nou2')
-    button1.addEventListener('click', e => {
-        if(numar == 1){
-            var text1 = 'winner';
-            document.write(text1);
+return Math.floor(Math.random() * max);
+ }
+var numar = getRandomInt(3);
+for( var i = 0; i < 3; ++i){
+    var btnNou = document.createElement('BUTTON');
+     btnNou.innerText = i;
+     btnNou.setAttribute("id", i)
+      adaugaButoate.appendChild(btnNou);
+        btnNou.addEventListener('click',  function() {
+        if(this.id == numar){
+            document.write("winner");
         }
         else{
-            alert('try again!')
+            alert("try again!");
         }
-    })
-    var button2 = document.getElementById('nou3')
-    button2.addEventListener('click', e => {
-        if(numar == 2){
-            var text2 = 'winner';
-            document.write(text2);
-        }
-        else{
-            alert('try again');
-        }
-    })
+    });
+}  
