455635c107a20ef7d0d73e64bd96b0ae63fc94da
IonutCampian <87191750+IonutCampian@users.noreply.github.com>
2021-08-05 21:08:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-var adaugaButoate = document.getElementById('adaugaButoate');
+var addButtons = document.getElementById('addButtons');
 var btn = [0 , 1 , 2];
 function getRandomInt(max) {
 return Math.floor(Math.random() * max);
  }
-var numar = getRandomInt(3);
+var number = getRandomInt(3);
 for( var i = 0; i < 3; ++i){
-    var btnNou = document.createElement('BUTTON');
-     btnNou.innerText = i;
-     btnNou.setAttribute("id", i)
-      adaugaButoate.appendChild(btnNou);
-        btnNou.addEventListener('click',  function() {
-        if(this.id == numar){
+    var newBtn = document.createElement('BUTTON');
+     newBtn.innerText = i;
+     newBtn.setAttribute("id", i)
+     addButtons.appendChild(newBtn);
+     newBtn.addEventListener('click',  function() {
+     if(this.id == number){
             document.write("winner");
-        }
-        else{
+     }
+     else{
             alert("try again!");
-        }
+     }
     });
 }  
